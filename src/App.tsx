8ff4ebf9ae0cdaca78e852681c788d26d532bc04
Zülal Nebin <zllnbn1997@gmail.com>
2022-05-23 15:25:28 +0300

    sign up page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,33 @@
-import "./styles.css";
+import React from "react";
+import { Route } from "react-router-dom";
+import SignUp from "./components/SignUp";
+import { Layout, Menu, Breadcrumb } from "antd";
+
+const { Header, Content, Footer } = Layout;
 
 export default function App() {
-  return <>Expense Tracker</>;
+  return (
+    <Layout>
+      <Header style={{ position: "fixed", zIndex: 1, width: "100%" }}>
+        <div className="logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={["2"]}
+          items={new Array(3).fill(null).map((_, index) => ({
+            key: String(index + 1),
+            label: `nav ${index + 1}`
+          }))}
+        />
+      </Header>
+      <Content
+        className="site-layout"
+        style={{ padding: "50px", marginTop: 64 }}>
+        <Route path="/register" component={SignUp} />
+      </Content>
+      <Footer style={{ textAlign: "center" }}>
+        Expense Tracker @ React Dersleri 2022
+      </Footer>
+    </Layout>
+  );
 }
