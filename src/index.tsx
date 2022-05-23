f67eff640498a99a17c26a474a15283df8958469
Zülal Nebin <zllnbn1997@gmail.com>
2022-05-23 10:34:51 +0300

    install packages and create folder structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,5 @@
-import { StrictMode } from "react";
-import * as ReactDOMClient from "react-dom/client";
-
+import ReactDOM from "react-dom";
 import App from "./App";
+import "antd/dist/antd.css";
 
-const rootElement = document.getElementById("root");
-const root = ReactDOMClient.createRoot(rootElement);
-
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+ReactDOM.render(<App />, document.getElementById("root"));
